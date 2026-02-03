98d7200db47826a090a24fea4341e71a6616bb60
KoreanBadboy <giang_hoang@hotmail.com>
2026-02-03 18:06:08 +0100

    Deploy update

--- GiangHoangPortfolio.WASM/Models/Journey.cs
+++ GiangHoangPortfolio.WASM/Models/Journey.cs
@@ -2,11 +2,11 @@ namespace GiangHoangPortfolio.WASM.Models;
 
 public class JourneyItem
 {
-    public string Title { get; set; }
-    public string Date { get; set; }
-    public string Status { get; set; }
-    public string Type { get; set; } // Course, Project, Milestone
+    public string? Title { get; set; }
+    public string? Date { get; set; }
+    public string? Status { get; set; }
+    public string? Type { get; set; } // Course, Project, Milestone
     public string? Score { get; set; }
-    public List<string> Tags { get; set; }
+    public List<string>? Tags { get; set; }
     public string? Description { get; set; }
 }
