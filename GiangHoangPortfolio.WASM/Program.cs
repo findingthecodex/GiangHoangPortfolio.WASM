996516ec4e89acbf1c353fb4c9262ea50f06151a
Vincent Wigardt <vincent.wigardt@gmail.com>
2026-02-04 11:00:29 +0100

    Add CounterService, CookieConsent, QR page, Counter page, and CV download functionality

--- GiangHoangPortfolio.WASM/Program.cs
+++ GiangHoangPortfolio.WASM/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GiangHoangPortfolio.WASM;
+using GiangHoangPortfolio.WASM.Services;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<CounterService>();
 
 await builder.Build().RunAsync();
