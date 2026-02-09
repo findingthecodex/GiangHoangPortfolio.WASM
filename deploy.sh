0ed86ffdad7c2ce03cc7332b85c8d2b137654598
Ditt namn <din.email@example.com>
2026-02-09 10:37:56 +0100

    Deploy update

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 
 echo "🔨 Building project..."
 cd GiangHoangPortfolio.WASM
-dotnet publish -c Release
+dotnet publish -c Release -o ./publish
 
 if [ $? -ne 0 ]; then
     echo "❌ Build failed!"
@@ -13,11 +13,10 @@ fi
 
 echo "🧹 Cleaning old files in root..."
 cd ..
-rm -rf _framework css _content *.css index.html 404.html favicon.png icon-192.png images sample-data
+rm -rf _framework css _content *.css *.html *.png images sample-data
 
 echo "📦 Copying new files from publish folder..."
-# Use the correct .NET version folder (net8.0)
-cp -r GiangHoangPortfolio.WASM/bin/Release/net8.0/publish/wwwroot/* .
+cp -r GiangHoangPortfolio.WASM/publish/wwwroot/* .
 
 echo "📄 Creating 404.html from index.html..."
 cp index.html 404.html
